693e48d8fe46a128017148dbcf0090d906530bd0
João Carlos <juniorbezerra1999@hotmail.com>
2025-01-31 00:09:21 -0300

    Alteração atividade 4

--- 4.atividade-nodejs/server.js
+++ 4.atividade-nodejs/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 
 //Rota Principal
@@ -12,10 +13,23 @@ app.get('/sobre', (req, res) => {
     res.send('<h1>Sobre</h1><p>Este é um servidor de teste.</p>');
 });
 
-app.get('/contato', (req, res) => {
-    res.send('<hq>Contato</h1><p>Entre em contato conosco pelo email: <a href="mailto:joao@gmail.com">meu_email</a> </p>')
-})
 
+//Rota de contato
+app.get('/contato', (req, res) => {
+    res.send('<hq>Contato</h1><p>Entre em contato conosco pelo email: <a href="mailto:joao@gmail.com">meu_email</a> </p>');
+});
+
+
+// Rota de informações
+app.post('/dados', (req, res) => {
+    const { nome, idade, email } = req.body;
+    res.send(`O seu nome é ${nome} você tem ${idade} anos e seu é email é ${email}`);
+});
+
+app.post('/informacao', (req, res) => {
+    const { nome, idade, sintomas } = req.body;
+    res.send(`O nome do paciente é ${nome}, ele tem ${idade} anos e o sintoma é: ${sintomas}`)
+})
 
 const port = 3000;
 app.listen(port, () => {
